86592a3b85d946c4a8242da7798f56da70124bd3
hanhanz <hanhanz@users.noreply.github.com>
2022-06-06 02:21:28 +0000

    deploy: d6563e2de24149a676c7854271384c63921cb83b

---
+++ precache-manifest.b84513e56732bef5aa89723020ecac33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d693d3308b1d759fd5a2d12e7201dcfe",
+    "url": "https://hanhanz.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "f8ea5314a4214b115fd2",
+    "url": "https://hanhanz.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "0e81c665ace42e908c00",
+    "url": "https://hanhanz.github.io/task-management-frontend/static/js/2.aa39f788.chunk.js"
+  },
+  {
+    "revision": "f8ea5314a4214b115fd2",
+    "url": "https://hanhanz.github.io/task-management-frontend/static/js/main.0bfe93ff.chunk.js"
+  },
+  {
+    "revision": "32354b4db5c31e1e853d",
+    "url": "https://hanhanz.github.io/task-management-frontend/static/js/runtime~main.8bf2defa.js"
+  }
+]);
